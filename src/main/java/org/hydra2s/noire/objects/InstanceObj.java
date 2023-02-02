ced95a5be6d8d09cf374dad6984d88128a3e999c
unit-a-user <helixd.2s18@gmail.com>
2023-02-02 20:33:36 +0800

    disable layers

--- src/main/java/org/hydra2s/noire/objects/InstanceObj.java
+++ src/main/java/org/hydra2s/noire/objects/InstanceObj.java
@@ -124,7 +124,7 @@ public class InstanceObj extends BasicObj {
                 .pApplicationInfo(this.appInfo.get(0))
                 .ppEnabledExtensionNames(this.extensions)
                 // validator is BROKEN!
-                .ppEnabledLayerNames(this.layers)
+                //.ppEnabledLayerNames(this.layers)
                 .get();
         vkCheckStatus(VK10.vkCreateInstance(this.instanceInfo, null, (this.handle = new Handle("Instance")).ptr()));
 
