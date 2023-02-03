1cd109ff266bd9c8da82093e094f23ddb5b34f20
unit-a-user <helixd.2s18@gmail.com>
2023-02-04 01:56:59 +0800

    add data downloader

--- src/main/java/org/hydra2s/noire/objects/CommandManagerObj.java
+++ src/main/java/org/hydra2s/noire/objects/CommandManagerObj.java
@@ -110,13 +110,177 @@ public class CommandManagerObj extends BasicObj {
     }
 
     //
-    public static class CommandProfiler {
-        public LongBuffer queryPool = null;
+    public static class CommandWriterBase {
+        public CommandManagerObj manager = null;
+        public ArrayList<UtilsCInfo.Pair<String, Function<VkCommandBuffer, VkCommandBuffer>>> callers = null;
+        public ArrayList<VirtualAllocation> allocations = null;
+        public ArrayList<Runnable> toFree = null;
+        public ArrayList<CommandAgent> agents = null;
+
+        //
+        public CommandWriterBase(CommandManagerObj manager, CommandWriterCInfo cInfo) {
+            this.manager = manager;
+            this.callers = new ArrayList<>();
+            this.allocations = new ArrayList<>();
+            this.toFree = new ArrayList<>();
+            this.agents = new ArrayList<>();
+        }
+
+        // TODO: correct naming
+        protected CommandWriterBase cmdAdd$(String typeName, Function<VkCommandBuffer, VkCommandBuffer> caller) {
+            if (caller != null) {
+                this.callers.add(this.callers.size(), new UtilsCInfo.Pair<>(typeName, caller));
+            }
+            return this;
+        }
+
+        // TODO: correct naming
+        public CommandWriterBase cmdAdd(String typeName, Function<VkCommandBuffer, VkCommandBuffer> caller) {
+            return this.cmdAdd$(typeName, caller);
+        }
+
+        // TODO: correct naming
+        public CommandWriterBase cmdAdd(String typeName, Function<VkCommandBuffer, VkCommandBuffer> caller, CommandAgent cmdAgent) {
+            cmdAgent.cmdAdd(typeName, caller);
+            agents.add(cmdAgent);
+            return this;
+        }
+
+        //
+        public CommandWriterBase cmdWrite(VkCommandBuffer commandBuffer) {
+            callers.forEach((run)->{
+                run.second.apply(commandBuffer);
+            });
+            callers.clear();
+            return this;
+        }
+
+        //
+        public DeviceObj.FenceProcess submitOnce(DeviceObj.SubmitCmd cmd) throws Exception {
+            if (this.callers.size() == 0) { return null; };
+
+            //
+            var toFreeFn = new ArrayList<>(this.toFree); this.toFree.clear();
+            var toFreeAlloc = new ArrayList<>(this.allocations);this.allocations.clear();
+            var toFreeAgents = new ArrayList<>(this.agents); this.agents.clear();
+            var callerCount = this.callers.size();
+
+            //
+            cmd.onDone = cmd.onDone != null ? cmd.onDone : new Promise();
+            cmd.onDone.thenApply((status)->{
+                // debug and profiling info
+                //System.out.println("Begin collect data and free command resources.");
+                //System.out.println("Command writes: " + callerCount);
+                //System.out.println("Command free resources: " + toFreeAlloc.size());
+                //System.out.println("Command free commands (include profiling): " + toFreeFn.size());
+
+                // free resources and collect profiling data
+                toFreeFn.forEach(Runnable::run); toFreeFn.clear();
+                toFreeAlloc.forEach(VirtualAllocation::free); toFreeAlloc.clear();
+
+                // collect timings of commands
+                //AtomicLong fullCommandTime = new AtomicLong(0L);
+                //toFreeProfilers.forEach((profiler)->{
+                //fullCommandTime.addAndGet(profiler.timeDiff);
+                //}); toFreeProfilers.clear();
+
+                // debug and profiling info
+                //System.out.println("Full commands time: " + ((double)fullCommandTime.get())/(double)(1000*1000) + "in milliseconds.");
+                //System.out.println("End collect data and free command resources.");
+
+                //
+                return status;
+            });
+
+            //
+            var fence = manager.deviceObj.submitOnce(cmd, (cmdBuf)->{
+                cmdWrite(cmdBuf);
+                return null;
+            });
+
+            manager.deviceObj.doPolling();
+            return fence;
+
+            // DEBUG MODE!
+            /*AtomicReference<DeviceObj.FenceProcess> last = new AtomicReference();
+            this.callers.forEach((fn)->{
+                DeviceObj.FenceProcess fence = null;
+
+                //
+                System.out.println("Command Type Name: " + fn.first);
+                System.out.println("Command Sequence ID: " + callers.indexOf(fn) + " of " + callers.size());
+
+                //
+                try {
+                    fence = manager.deviceObj.submitOnce(new DeviceObj.SubmitCmd(){{
+                        queueGroupIndex = cmd.queueGroupIndex;
+                        commandPoolIndex = cmd.commandPoolIndex;
+                        if (callers.indexOf(fn) == (callers.size()-1)) {
+                            waitSemaphoreSubmitInfo = cmd.waitSemaphoreSubmitInfo;
+                            signalSemaphoreSubmitInfo = cmd.signalSemaphoreSubmitInfo;
+                            onDone = cmd.onDone != null ? cmd.onDone : new Promise();
+                            onDone.thenApply((status)->{
+                                toFreeAlloc.forEach(VirtualAllocation::free); toFreeAlloc.clear();
+                                toFreeFn.forEach(Runnable::run); toFreeFn.clear();
+                                return status;
+                            });
+                        }
+                    }}, fn.second);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+
+                //
+                manager.deviceObj.doPolling();
+
+                //
+                if (fence.fence != null && fence.fence.get(0) != 0) {
+                    vkCheckStatus(vkWaitForFences(manager.deviceObj.device, fence.fence, true, 9007199254740991L));
+                };
+
+                //
+                if (callers.indexOf(fn) == (callers.size()-1)) {
+                    last.set(fence);
+                }
+            });
+            this.callers.clear();
+            return last.get();*/
+
+
+
+        }
+
+        public CommandWriterBase addToFree(Runnable fn) {
+            this.toFree.add(fn);
+            return this;
+        }
+
+    }
+
+    //
+    public static class CommandAgent {
         public CommandManagerObj manager = null;
         public CommandWriter commandWriter = null;
+
+        public CommandAgent cmdAdd(String typeName, Function<VkCommandBuffer, VkCommandBuffer> caller) {
+            commandWriter.cmdAdd$(typeName, caller);
+            return this;
+        }
+
+        public CommandAgent(CommandManagerObj manager, CommandWriter commandWriter){
+            this.manager = manager;
+            this.commandWriter = commandWriter;
+        }
+    }
+
+    // TODO: command agents support
+    public static class CommandProfiler extends CommandAgent {
+        public LongBuffer queryPool = null;
+
         public long timeDiff = 0L;
 
         public CommandProfiler(CommandManagerObj manager, CommandWriter commandWriter) {
+            super(manager, commandWriter);
             this.queryPool = memAllocLong(1);
 
             //
@@ -126,12 +290,12 @@ public class CommandManagerObj extends BasicObj {
             queryPoolCreateInfo.queryCount(2);
 
             //
-            this.manager = manager;
-            this.commandWriter = commandWriter;
             vkCreateQueryPool(manager.deviceObj.device, queryPoolCreateInfo, null, this.queryPool);
         }
 
+        @Override
         public CommandProfiler cmdAdd(String typeName, Function<VkCommandBuffer, VkCommandBuffer> caller) {
+
             commandWriter.cmdAdd$("Command Profiler Begin Record", (cmdBuf)->{
                 vkCmdWriteTimestamp(cmdBuf, VK_PIPELINE_STAGE_ALL_COMMANDS_BIT, this.queryPool.get(0), 0);
                 return cmdBuf;
@@ -154,37 +318,91 @@ public class CommandManagerObj extends BasicObj {
     }
 
     //
-    public static class CommandWriter {
-        public CommandManagerObj manager = null;
-        public ArrayList<UtilsCInfo.Pair<String, Function<VkCommandBuffer, VkCommandBuffer>>> callers = null;
-        public ArrayList<VirtualAllocation> allocations = null;
-        public ArrayList<Runnable> toFree = null;
-        public ArrayList<CommandProfiler> profilers = null;
+    public static class DataDownloader extends CommandWriterBase {
 
         //
+        public DataDownloader(CommandManagerObj manager, CommandWriterCInfo cInfo) {
+            super(manager, cInfo);
+        }
+
+        //
+        public DeviceObj.FenceProcess cmdCopyFromImageToHost(Callable<HostImageStage> imageInfoLazy, ByteBuffer data, DeviceObj.SubmitCmd cmd) throws Exception {
+            AtomicReference<VirtualAllocation> allocation_ = new AtomicReference<>();
+            AtomicReference<HostImageStage> imageInfo_ = new AtomicReference<>();
+
+            //
+            Callable<Integer> tempOp = ()-> {
+                allocation_.set(new VirtualAllocation(this.manager.virtualBlock.get(0), data.remaining()));
+                var allocation = allocation_.get(); var status = allocation.getStatus();
+                this.allocations.add(allocation);
+                if (status == 0) {
+                    var allocOffset = allocation.offset.get(0);
+                } else {
+                    System.out.println("Allocation Failed: " + status + ", memory probably ran out...");
+                    throw new Exception("Allocation Failed: " + status + ", memory probably ran out...");
+                }
+                return status;
+            };
+
+            //
+            boolean lazy = false;
+
+            //
+            AtomicInteger status = new AtomicInteger(-2);
+            if (!lazy) { status.set(tempOp.call()); };
+            this.cmdAdd("Image To Host Copy", (cmdBuf)->{
+                if (lazy) {
+                    try {
+                        status.set(tempOp.call());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                };
+                var allocation = allocation_.get();
+                if (status.get() == 0) {
+                    try {
+                        imageInfo_.set(imageInfoLazy.call());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    var allocOffset = allocation.offset.get(0);
+                    var imageInfo = imageInfo_.get();
+
+                    if (allocation.range <= 0L) {
+                        throw new RuntimeException("Command Writer Error (Image To Host): Bad Dst Range");
+                    };
+
+                    //
+                    CommandUtils.cmdCopyImageToBuffer(cmdBuf, imageInfo.image, new CommandUtils.BufferCopyInfo() {{
+                        buffer = manager.bufferHeap.getHandle().get();
+                        offset = allocOffset;
+                        range = allocation.range;
+                        rowLength = imageInfo.rowLength;
+                        imageHeight = imageInfo.imageHeight;
+                    }}, imageInfo.extent3D);
+                }
+                return cmdBuf;
+            });
+
+            //
+            this.addToFree(()->{
+                var allocation = allocation_.get();
+                if (status.get() == 0) {
+                    var allocOffset = allocation.offset.get(0);
+                    memCopy(manager.bufferHeap.map(allocation.range, allocOffset), data);
+                }
+            });
+
+            //
+            return this.submitOnce(cmd);
+        }
+    }
+
+    //
+    public static class CommandWriter extends CommandWriterBase {
+
         public CommandWriter(CommandManagerObj manager, CommandWriterCInfo cInfo) {
-            this.manager = manager;
-            this.callers = new ArrayList<>();
-            this.allocations = new ArrayList<>();
-            this.toFree = new ArrayList<>();
-            this.profilers = new ArrayList<>();
-        }
-
-        // TODO: correct naming
-        protected CommandWriter cmdAdd$(String typeName, Function<VkCommandBuffer, VkCommandBuffer> caller) {
-            if (caller != null) {
-                this.callers.add(this.callers.size(), new UtilsCInfo.Pair<>(typeName, caller));
-            }
-            return this;
-        }
-
-        // TODO: correct naming
-        public CommandWriter cmdAdd(String typeName, Function<VkCommandBuffer, VkCommandBuffer> caller) {
-            return this.cmdAdd$(typeName, caller);
-            //var profilerObj = new CommandProfiler(this.manager, this);
-            //profilerObj.cmdAdd(typeName, caller);
-            //profilers.add(profilerObj);
-            //return this;
+            super(manager, cInfo);
         }
 
         //
@@ -330,114 +548,7 @@ public class CommandManagerObj extends BasicObj {
             return this;
         }*/
 
-        //
-        public CommandWriter cmdWrite(VkCommandBuffer commandBuffer) {
-            callers.forEach((run)->{
-                run.second.apply(commandBuffer);
-            });
-            callers.clear();
-            return this;
-        }
 
-        //
-        public DeviceObj.FenceProcess submitOnce(DeviceObj.SubmitCmd cmd) throws Exception {
-            if (this.callers.size() == 0) { return null; };
-
-            //
-            var toFreeFn = new ArrayList<>(this.toFree); this.toFree.clear();
-            var toFreeAlloc = new ArrayList<>(this.allocations);this.allocations.clear();
-            var toFreeProfilers = new ArrayList<>(this.profilers); this.profilers.clear();
-            var callerCount = this.callers.size();
-
-            //
-            cmd.onDone = cmd.onDone != null ? cmd.onDone : new Promise();
-            cmd.onDone.thenApply((status)->{
-                // debug and profiling info
-                //System.out.println("Begin collect data and free command resources.");
-                //System.out.println("Command writes: " + callerCount);
-                //System.out.println("Command free resources: " + toFreeAlloc.size());
-                //System.out.println("Command free commands (include profiling): " + toFreeFn.size());
-
-                // free resources and collect profiling data
-                toFreeAlloc.forEach(VirtualAllocation::free); toFreeAlloc.clear();
-                toFreeFn.forEach(Runnable::run); toFreeFn.clear();
-
-                // collect timings of commands
-                //AtomicLong fullCommandTime = new AtomicLong(0L);
-                //toFreeProfilers.forEach((profiler)->{
-                    //fullCommandTime.addAndGet(profiler.timeDiff);
-                //}); toFreeProfilers.clear();
-
-                // debug and profiling info
-                //System.out.println("Full commands time: " + ((double)fullCommandTime.get())/(double)(1000*1000) + "in milliseconds.");
-                //System.out.println("End collect data and free command resources.");
-
-                //
-                return status;
-            });
-
-            //
-            var fence = manager.deviceObj.submitOnce(cmd, (cmdBuf)->{
-                cmdWrite(cmdBuf);
-                return null;
-            });
-
-            manager.deviceObj.doPolling();
-            return fence;
-
-            // DEBUG MODE!
-            /*AtomicReference<DeviceObj.FenceProcess> last = new AtomicReference();
-            this.callers.forEach((fn)->{
-                DeviceObj.FenceProcess fence = null;
-
-                //
-                System.out.println("Command Type Name: " + fn.first);
-                System.out.println("Command Sequence ID: " + callers.indexOf(fn) + " of " + callers.size());
-
-                //
-                try {
-                    fence = manager.deviceObj.submitOnce(new DeviceObj.SubmitCmd(){{
-                        queueGroupIndex = cmd.queueGroupIndex;
-                        commandPoolIndex = cmd.commandPoolIndex;
-                        if (callers.indexOf(fn) == (callers.size()-1)) {
-                            waitSemaphoreSubmitInfo = cmd.waitSemaphoreSubmitInfo;
-                            signalSemaphoreSubmitInfo = cmd.signalSemaphoreSubmitInfo;
-                            onDone = cmd.onDone != null ? cmd.onDone : new Promise();
-                            onDone.thenApply((status)->{
-                                toFreeAlloc.forEach(VirtualAllocation::free); toFreeAlloc.clear();
-                                toFreeFn.forEach(Runnable::run); toFreeFn.clear();
-                                return status;
-                            });
-                        }
-                    }}, fn.second);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-
-                //
-                manager.deviceObj.doPolling();
-
-                //
-                if (fence.fence != null && fence.fence.get(0) != 0) {
-                    vkCheckStatus(vkWaitForFences(manager.deviceObj.device, fence.fence, true, 9007199254740991L));
-                };
-
-                //
-                if (callers.indexOf(fn) == (callers.size()-1)) {
-                    last.set(fence);
-                }
-            });
-            this.callers.clear();
-            return last.get();*/
-
-
-
-        }
-
-        public CommandWriter addToFree(Runnable fn) {
-            this.toFree.add(fn);
-            return this;
-        }
     };
 
     //
@@ -473,6 +584,11 @@ public class CommandManagerObj extends BasicObj {
     //
     public CommandWriter makeCmdWriter(CommandWriterCInfo cInfo) {
         return new CommandWriter(this, cInfo);
+    }
+
+    //
+    public DataDownloader makeCmdDownloader(CommandWriterCInfo cInfo) {
+        return new DataDownloader(this, cInfo);
     }
 
 }
