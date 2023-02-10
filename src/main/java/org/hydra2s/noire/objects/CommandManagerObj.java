3be110c2bff3b181c8c2dcbb7aeab722c5a4a0f1
unit-a-user <helixd.2s18@gmail.com>
2023-02-10 08:24:02 +0800

    no possible to clear resources

--- src/main/java/org/hydra2s/noire/objects/CommandManagerObj.java
+++ src/main/java/org/hydra2s/noire/objects/CommandManagerObj.java
@@ -142,15 +142,12 @@ public class CommandManagerObj extends BasicObj {
 
         // when skip rendering, needs to free unused resources
         public CommandWriterBase clear() {
-            /*var toFreeFn = new ArrayList<>(this.toFree); this.toFree.clear();
-            var toFreeAlloc = new ArrayList<>(this.allocations);this.allocations.clear();
-            var toFreeAgents = new ArrayList<>(this.agents); this.agents.clear();
-            var callerCount = this.callers.size();
+            if (this.callers.size() == 0) { return null; };
             {
                 // free resources and collect profiling data
-                toFreeFn.forEach(Runnable::run); toFreeFn.clear();
-                toFreeAlloc.forEach(VirtualAllocation::free); toFreeAlloc.clear();
-            }*/
+                //toFree.forEach(Runnable::run); toFree.clear();
+                //allocations.forEach(VirtualAllocation::free); allocations.clear();
+            }
             this.callers.clear();
             return this;
         }
